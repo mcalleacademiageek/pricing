853a1efcf33297b4bda3b69a7f9c40168a1c67aa
mcalleacademiageek <maodesarrollowebacademiageek@gmail.com>
2020-11-24 11:49:39 -0500

    se crea proyecto pricing

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import PricingPage from './pages/PricingPage';
+
+ReactDOM.render(
+  <PricingPage/>,
+  document.getElementById('root')
+);
+
